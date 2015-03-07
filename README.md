a1403d1d999a7235b3f9c674ba15ae15848ac57c
Andrei Vacariu <andrei@avacariu.me>
2015-03-06 21:38:41 -0800

    Add impl for &str

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # rust-changecase [![Build Status](https://travis-ci.org/vlad003/rust-changecase.svg)](https://travis-ci.org/vlad003/rust-changecase)
 
-Change the case of Strings in Rust.
+Change the case of Strings in Rust. It's also implemented for `&str`
 
 Available functions:
 
@@ -12,7 +12,11 @@ Available functions:
 What I'm working on:
 
 * Title Case
-* Implementation for `&str`
+
+**Note:** The implementation for `&str` returns String. I'd definitely prefer
+for it to return `&str` but I noticed that `std::ascii:AsciiExt` also returns a
+String for `&str`, so I went with that. I'm also not really sure how to get the
+functions to return `&str` :confused:
 
 ## Examples
 
@@ -26,3 +30,10 @@ What I'm working on:
     assert_eq!(String::from_str("some thing").to_capitalized(), "Some thing");
     assert_eq!(String::from_str("Some").to_altcase(Case::Lower), "sOmE");
     assert_eq!(String::from_str("Some").to_altcase(Case::Upper), "SoMe");
+
+    assert_eq!("Some".to_uppercase(), "SOME");
+    assert_eq!("Some".to_lowercase(), "some");
+    assert_eq!("Some".to_invertedcase(), "sOME");
+    assert_eq!("some thing".to_capitalized(), "Some thing");
+    assert_eq!("Some".to_altcase(Case::Lower), "sOmE");
+    assert_eq!("Some".to_altcase(Case::Upper), "SoMe");
--- src/lib.rs
+++ src/lib.rs
@@ -7,14 +7,14 @@ pub enum Case {
 }
 
 pub trait ChangeCase {
-    fn to_uppercase(&self) -> Self;
-    fn to_lowercase(&self) -> Self;
-    fn to_capitalized(&self) -> Self;
-    fn to_invertedcase(&self) -> Self;
-    fn to_altcase(&self, start_with: Case) -> Self;
+    fn to_uppercase<'a>(&'a self) -> String;
+    fn to_lowercase<'a>(&'a self) -> String;
+    fn to_capitalized<'a>(&'a self) -> String;
+    fn to_invertedcase<'a>(&'a self) -> String;
+    fn to_altcase<'a>(&'a self, start_with: Case) -> String;
 }
 
-impl ChangeCase for String {
+impl<'a> ChangeCase for String {
     fn to_uppercase(&self) -> String {
         if self.is_empty() {
             return String::new();
@@ -121,6 +121,25 @@ impl ChangeCase for String {
     }
 }
 
+impl<'a> ChangeCase for &'a str {
+    fn to_uppercase(&self) -> String {
+        String::from_str(self).to_uppercase()
+    }
+    fn to_lowercase(&self) -> String {
+        String::from_str(self).to_lowercase()
+    }
+    fn to_capitalized(&self) -> String {
+        String::from_str(self).to_capitalized()
+    }
+    fn to_invertedcase(&self) -> String {
+        String::from_str(self).to_invertedcase()
+    }
+    fn to_altcase(&self, start_with: Case) -> String {
+        String::from_str(self).to_altcase(start_with)
+    }
+}
+
+
 #[test]
 fn upper() {
     assert_eq!(String::from_str("Something").to_uppercase(), "SOMETHING");
@@ -156,4 +175,34 @@ fn alternating() {
     assert_eq!(String::from_str("som3thing").to_altcase(Case::Lower), "sOm3ThInG");
     assert_eq!(String::from_str("som3thing").to_altcase(Case::Upper), "SoM3tHiNg");
     assert_eq!(String::from_str("some word").to_altcase(Case::Lower), "sOmE wOrD");
+}
+
+#[test]
+fn str_lower() {
+    assert_eq!("SOMETHING".to_lowercase(), "something");
+    assert_eq!("sOmEtHiNg".to_lowercase(), "something");
+    assert_eq!("Word WORD2".to_lowercase(), "word word2");
+}
+
+#[test]
+fn str_capitalize() {
+    assert_eq!("SOMETHING".to_capitalized(), "Something");
+    assert_eq!("sOmEtHiNg".to_capitalized(), "Something");
+    assert_eq!("Word WORD2".to_capitalized(), "Word word2");
+}
+
+#[test]
+fn str_swapped() {
+    assert_eq!("SOMETHING".to_invertedcase(), "something");
+    assert_eq!("SoMeThInG".to_invertedcase(), "sOmEtHiNg");
+    assert_eq!("Some WorD2".to_invertedcase(), "sOME wORd2");
+}
+
+#[test]
+fn str_alternating() {
+    assert_eq!("SOMETHING".to_altcase(Case::Lower), "sOmEtHiNg");
+    assert_eq!("SOMETHING".to_altcase(Case::Upper), "SoMeThInG");
+    assert_eq!("som3thing".to_altcase(Case::Lower), "sOm3ThInG");
+    assert_eq!("som3thing".to_altcase(Case::Upper), "SoM3tHiNg");
+    assert_eq!("some word".to_altcase(Case::Lower), "sOmE wOrD");
 }
